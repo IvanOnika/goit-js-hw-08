573c7c92f58694d8f92e2299859c8480afda59e8
Ivan Onika <vanyaonika@gmail.com>
2023-01-29 15:15:40 +0200

    1 exersice done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
+
+const createItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+  })
+  .join('');
+
+const galleryContainerEl = document.querySelector('.gallery');
+galleryContainerEl.insertAdjacentHTML('beforeend', createItemsMarkup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+  doubleTapZoom: 1,
+});
